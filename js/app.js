606be4efaee2267ef87679d5000276b63443f4d3
hoovercj <hoovercj@iastate.edu>
2014-12-10 04:19:27 +0100

    Moved misc. column to a hidden row that shows/hides on an icon click

--- js/app.js
+++ js/app.js
@@ -6,4 +6,11 @@
 
 $(document).ready(function() {
     $("img").unveil(50);
+    $('.misc-toggle').click(function() {
+        //$(this.hash).slideToggle(500);
+        $(this).find('i').toggleClass('down up')
+        $(this).parent().next('.collapse').find('div').slideToggle(500);
+        //$('#row-'+this.id).slideToggle(500);
+        return false;        
+    });
 });
