64311314f64ebf6c4cb62833fcd2a545d7f73a61
hoovercj <hoovercj@iastate.edu>
2015-03-05 23:07:47 +0100

    fixed form on test page

--- js/app.js
+++ js/app.js
@@ -4,27 +4,60 @@
     $('a.popup.exception').popup();
 }(window, jQuery));
 
-var resetPasswordUrl = 'https://password-resetter.herokuapp.com/resetpassword/';
-var resetPasswordTestUrl = 'https://password-resetter.herokuapp.com/resetpassword/test/';
+var resetPasswordUrl = 'https://phantom-password-resetter.herokuapp.com/resetpassword/';
+var resetPasswordTestUrl = 'https://phantom-password-resetter.herokuapp.com/resetpassword/test/';
 
 $(document).ready(function() {
-    $("img").unveil(50);
+    $('img').unveil(50);
     $('.misc-toggle').click(function() {
         $(this).find('i').toggleClass('down up')
         $(this).parent().next('.collapse').find('div').slideToggle(500);
         return false;        
     });
+    $('#reset-message-close-icon').click(function() {
+    	$(this).parent().hide();
+    })
+
+    var showMessage = function (messageClass, messageHeader, messageBody) {
+    	$('#reset-message').removeClass('positive negative');
+    	$('#reset-message').addClass(messageClass);
+    	$('#reset-message-header').text(messageHeader);
+    	$('#reset-message-body').text(messageBody);
+    	$('#reset-message').show();
+    }
 
     $('#reset-password-button').click(function() {
     	var email = $('#reset-password-input').val();
-    	$.get( resetPasswordUrl + email, function(data) {});
+    	$(this).addClass('loading');
+    	$.get( resetPasswordUrl + email, function(data) {
+    		console.dir(data);    		
+    	}).fail(function(error) {
+    		console.dir(error);
+    	}).always(function() {
+    		$('this').removeClass('loading');    		
+    	});
     });
 
-    $('#reset-password-test-button').click(function() {
-        var email = $('#reset-password-email-input').val();
+    $('#test-form').on('submit', function (e) {
+		e.preventDefault();
+		console.log('OnSubmit');
+		var email = $('#reset-password-email-input').val();
         var site = $('#reset-password-site-input').val();
-        $.get( resetPasswordTestUrl + "?email=" + email + "&site=" + site, function(data) {
-            alert(data);
-        });
-    });
+        $('#reset-password-test-button').prop('disabled', 'disabled');
+        $('#reset-password-test-button').addClass('disabled');
+        $.get( resetPasswordTestUrl + '?email=' + email + '&site=' + site,  function(data) {
+        	console.dir(data);
+        	if(data.error) {
+        		showMessage('negative', 'Error', data.error);
+        	} else {
+        		showMessage('positive', 'Check your inbox', data.message);
+        	}
+    	}).fail(function(error) {
+    		console.dir(error);
+    		showMessage('negative', 'Error', error.status);    		
+    	}).always(function() {    		
+    		$('#reset-password-test-button').removeAttr('disabled');
+    		$('#reset-password-test-button').removeClass('disabled');
+    	});		
+	});
 });
